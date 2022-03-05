d9273fd2bfbc395c288cb928facc3f998ffa0d77
Alex Julian <alexjulian1227@gmail.com>
2022-03-05 22:41:46 +0800

    Add script.js

---
+++ jsFundamentals/script.js
@@ -0,0 +1,13 @@
+let js = "amazing";
+
+if (js === "amazing") {
+    console.log("Javascript is FUN");
+}
+
+else {
+    console.log("Javascript is BORING");
+}
+
+let firstName = "Alex";
+
+console.log(firstName)
