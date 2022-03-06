afded53561e930febb24183007ae7b735c31c3d3
Alex Julian <alexjulian1227@gmail.com>
2022-03-07 00:32:39 +0800

    Progress fundamentals js

--- jsFundamentals/script.js
+++ jsFundamentals/script.js
@@ -154,8 +154,129 @@
 
 //type conversion and coersion
 
-const inputYear = '1991';
-console.log(Number(inputYear))
-console.log(Number(inputYear) + 18);
+// const inputYear = '1991';
+// console.log(Number(inputYear))
+// console.log(Number(inputYear) + 18);
 
-console.log(String(23) + ' years old');
+// console.log(String(23) + ' years old');
+
+// //type coersion
+// console.log('I am ' + 23 + ' years old.')
+// //converts the data type automatically for the output
+
+//truthsy and faltsy
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Jonas'));
+// console.log(Boolean({}));
+
+// const age = 18;
+// // === strict comparison
+// if (age === 18) console.log('You are now on legal age');
+
+// const ageNew = '18';
+
+// if (ageNew == 18) console.log('Loose comparison that can use type coersion');
+
+// const favNum = Number(prompt("What's your favorite number ?"));
+
+// console.log(favNum);
+
+// if (favNum == 7) {
+//     console.log('Cool');
+// }
+// else {
+//     console.log('Loose comparison')
+// }
+
+// const hasDriverLicense = true;
+// const hasGoodVision = true;
+
+// console.log(hasDriverLicense && hasGoodVision);
+// console.log(hasDriverLicense || hasGoodVision);
+// console.log(!hasDriverLicense); 
+
+// const shouldDrive = hasDriverLicense && hasGoodVision;
+
+// if (shouldDrive) {
+//     console.log('You can drive now!');
+// } else {
+//     console.log('No!')
+// }
+
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each
+// other 3 times. The winner with the highest average score wins a trophy!
+// Your tasks:
+// 1. Calculate the average score for each team, using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition,
+// and print it to the console. Don't forget that there can be a draw, so test for that
+// as well (draw means they have the same average score)
+// 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+// team only wins if it has a higher score than the other team, and the same time a
+// score of at least 100 points. Hint: Use a logical operator to test for minimum
+// score, as well as multiple else-if blocks �
+// 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+// both teams have the same score and both have a score greater or equal 100
+// points. Otherwise, no team wins the trophy
+// Test data:
+// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+// const averageDolphins = (97 + 112 + 101) / 3;
+// const averageKoalas  = (109 + 95 + 106) / 3;
+
+// if (averageDolphins && averageKoalas < 100) {
+//     console.log(`No one wins the round! Scores below 100 
+//     Dolphins: ${averageDolphins}
+//     Koalas: ${averageKoalas}`);
+// }
+
+// else if (averageDolphins > averageKoalas && averageDolphins >= 100) {
+//     console.log(`Dolphins win this round! 
+//     Dolphins: ${averageDolphins}
+//     Koalas: ${averageKoalas}`);
+// } else if (averageKoalas > averageDolphins && averageKoalas >= 100) {
+//     console.log(`Koalas win this round! 
+//     Dolphins: ${averageDolphins}
+//     Koalas: ${averageKoalas}`);
+// } else {
+//     console.log(`Draw! 
+//     Dolphins: ${averageDolphins}
+//     Koalas: ${averageKoalas}`);
+// }
+//switch statement
+const day = 'saturday';
+
+switch (day) {
+    case 'monday':
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy weekend');
+        break;
+    default:
+        console.log('Not a valid day!')
+}
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+}
+else if (day === 'tuesday') console.log('Prepare theory videos');
+else if (day === 'wednesday' || day === 'thursday') console.log('Write code examples');
+else if (day === 'friday') console.log('Record videos');
+else if (day === 'saturday' || day === 'sunday') console.log('Enjoy weekend');
+else console.log('Not a valid day!');
