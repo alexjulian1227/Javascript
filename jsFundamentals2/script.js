be2c79d391420eeda0fd8838a3be5dced7a3f41d
Alex Julian <alexjulian1227@gmail.com>
2022-03-07 19:02:31 +0800

    Finished fundamentals 2 moving to dom manipulation

--- jsFundamentals2/script.js
+++ jsFundamentals2/script.js
@@ -1,4 +1,4 @@
-'use strict'; //this mode will help you debug codes easier
+"use strict"; //this mode will help you debug codes easier
 
 //function declaration
 // function logger() {
@@ -16,7 +16,6 @@
 // const appleOrangeJuice = fruitProcesser(2, 4);
 
 // console.log(appleOrangeJuice);
-
 
 // function calcAge1(birthYear) {
 //     return 2022 - birthYear;
@@ -72,7 +71,6 @@
 // Apply this to the team's average scores �
 
 // const calcAverage = (game1, game2, game3) => (game1 + game2 + game3) / 3;
-
 
 // const avgDolphins = calcAverage(85,54,41);
 // const avgKoalas = calcAverage(23,34,27);
@@ -204,7 +202,6 @@
 // const nameKey = 'Name';
 // console.log(`${jonas['first' + nameKey]} is ${jonas['age']} years old ${jonas['job']}.`);
 
-
 // let userChoice = prompt("What do you want to know about Jonas? Choose between firstName, lastName, age, job, friends");
 
 // console.log(`Here's the info you need: ${jonas[userChoice]}`);
@@ -265,36 +262,176 @@
 // Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
 // tall
 
-const mark = {
-    fullName: 'Mark Miller',
-    mass: 78,
-    height: 1.69,
-    calcBMI: function (){
-        this.BMI = this.mass / (this.height ** 2);
-        return this.BMI
-    }
-}
+// const mark = {
+//     fullName: 'Mark Miller',
+//     mass: 78,
+//     height: 1.69,
+//     calcBMI: function (){
+//         this.BMI = this.mass / (this.height ** 2);
+//         return this.BMI
+//     }
+// }
 
-const john = {
-    fullName: 'John Smith',
-    mass: 92,
-    height: 1.95,
-    calcBMI: function (){
-        this.BMI = this.mass / (this.height ** 2);
-        return this.BMI
-    }
-}
+// const john = {
+//     fullName: 'John Smith',
+//     mass: 92,
+//     height: 1.95,
+//     calcBMI: function (){
+//         this.BMI = this.mass / (this.height ** 2);
+//         return this.BMI
+//     }
+// }
 
-mark.calcBMI();
-john.calcBMI();
+// mark.calcBMI();
+// john.calcBMI();
 
-// console.log(`${mark.BMI > john.BMI ? mark.fullName : john.fullName}'s BMI
-// (${mark.BMI > john.BMI ? mark.BMI : john.BMI}) is higher than
-// `)
-console.log(`-------------------------------`)
-console.log(`${mark.BMI > john.BMI
-            ? `${mark.fullName}'s BMI ${mark.BMI} is higher than ${john.fullName}'s BMI ${john.BMI}.`
-            : `${john.fullName}'s BMI ${john.BMI} is higher than ${mark.fullName}'s BMI ${mark.BMI}.`
-}
-`
-);
+// // console.log(`${mark.BMI > john.BMI ? mark.fullName : john.fullName}'s BMI
+// // (${mark.BMI > john.BMI ? mark.BMI : john.BMI}) is higher than
+// // `)
+// console.log(`-------------------------------`)
+// console.log(`${mark.BMI > john.BMI
+//             ? `${mark.fullName}'s BMI ${mark.BMI} is higher than ${john.fullName}'s BMI ${john.BMI}.`
+//             : `${john.fullName}'s BMI ${john.BMI} is higher than ${mark.fullName}'s BMI ${mark.BMI}.`
+// }
+// `
+// );
+
+//loops
+
+//for
+// for(let rep = 1; rep <= 10; rep++) {
+//     console.log('Lifting weights repitition', rep);
+// }
+
+// const jonas = [
+//     'Jonas',
+//     'Schmedtmann',
+//     46,
+//     'teacher',
+//     ['Michael', 'Peter', 'Steven']
+// ];
+// const jonasTypes = [];
+// for(let i = 0; i < jonasArray.length;i++) {
+//     jonasTypes[i] = typeof(jonasArray[i]);
+// }
+// console.log(jonasArray)
+// console.log(jonasTypes);
+
+// for(let i = 0; i < jonasArray.length; i++) {
+//     console.log(`${jonasArray[i]}  -   ${jonasTypes[i]}`);
+// }
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+// for(let i = 0; i < years.length; i++) {
+//     ages.push(2022 - years[i]);
+// }
+// console.log(`Ages: [${ages}]`);
+
+//continue & breaks
+// for(let i = 0; i < jonasArray.length; i++) {
+//     if(typeof jonasArray[i] !== 'string') continue; //skip the non string types
+//         console.log(`${jonasArray[i]}`);
+// }
+
+// for(let i = 0; i < jonasArray.length; i++) {
+//     if(typeof jonasArray[i] === 'number') break; //loop will stop when found a numbertype
+//         console.log(`${jonasArray[i]}`);
+// }
+
+//loop backwards
+// for(let i = jonas.length - 1; i >= 0; i--){
+//     console.log(jonas[i]);
+// }
+//loop inside loop
+// for (let exercise = 1; exercise < 4; exercise++) {
+//     console.log(`------Starting exercise ${exercise}`);
+
+//     for (let rep = 1; rep < 6; rep++) {
+//         console.log(`Lifting weight repetition ${rep}`);
+//     }
+// }
+// for (let rep = 1; rep < 6; rep++) {
+//         console.log(`Lifting weight repetition ${rep}`);
+//     }
+
+//while loop
+// let rep = 1;
+// while (rep < 6) {
+//     console.log(`Lifting weight repetition ${rep}`);
+//     rep++;
+// }
+
+// let dice =  Math.trunc(Math.random() * 6) + 1;
+
+// while (dice !== 6) { //loop will run if the condition is TRUE
+//     console.log(`You rolled ${dice}.`);
+//     dice =  Math.trunc(Math.random() * 6) + 1;
+// }
+
+// console.log(`Finally! You got ${dice}!`)
+
+// Let's improve Steven's tip calculator even more, this time using loops!
+// Your tasks:
+// 1. Create an array 'bills' containing all 10 test bill values
+// 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+// 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+// tips and total values (bill + tip) for every bill value in the bills array. Use a for
+// loop to perform the 10 calculations!
+// Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+// Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+// tips and totals arrays �
+// Bonus:
+// 4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+// an argument. This function calculates the average of all numbers in the given
+// array. This is a difficult challenge (we haven't done this before)! Here is how to
+// solve it:
+// First, you will need to add up all values in the array. To do the addition,
+// start by creating a variable 'sum' that starts at 0. Then loop over the
+// array using a for loop. In each iteration, add the current value to the
+// 'sum' variable. This way, by the end of the loop, you have all values
+// added together
+// 4.2. To calculate the average, divide the sum you calculated before by the
+// length of the array (because that's the number of elements)
+// 4.3. Call the function with the 'totals' array
+
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
+
+// const calcTip = function (bill) {
+//   const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+//   const tot = bill + tip;
+//   return tot;
+// };
+
+// for (let i = 0; i < bills.length; i++) {
+//   totals[i] = calcTip(bills[i]);
+// }
+// console.log(`Bill: ${bills}`);
+
+// console.log(`Total w/ tip: ${totals}`);
+
+//Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+// an argument. This function calculates the average of all numbers in the given
+// array. This is a difficult challenge (we haven't done this before)! Here is how to
+// solve it:
+
+// let sum = 0;
+// const calcAverage = function (arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     // the program will loop if the condition is met i < arr.length
+//     sum = arr[i] + sum;
+//   }
+
+//   return sum / arr.length;
+// };
+
+// console.log(totals);
+// console.log(calcAverage(totals));
+
+const calcAge = (birthYear) => 2022 - birthYear;
+
+console.log(calcAge(1994));
+console.log(aa);
