ef5b3a668487d260d1bd77648cf8446899ed4dcc
Alex Julian <alexjulian1227@gmail.com>
2022-03-07 19:35:30 +0800

    Progress dom man

--- guessMyNumber/script.js
+++ guessMyNumber/script.js
@@ -1,1 +1,17 @@
-'use strict';
+"use strict";
+
+let message = document.querySelector(".message");
+let number = document.querySelector(".number");
+let score = document.querySelector(".score");
+let guess = document.querySelector(".guess");
+
+let generateNumber = Math.trunc(Math.random() * 20 + 1);
+let baseScore = 20;
+
+number.textContent = generateNumber;
+
+if (guess.value > generateNumber) {
+  message.textContent("Too high");
+} else {
+  message.textContent("Too low");
+}
