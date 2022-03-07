c3a81bbabb1a7387fbab1478fe2db2cc66ca2530
Alex Julian <alexjulian1227@gmail.com>
2022-03-07 14:45:50 +0800

    Progress fundamentals 2

--- jsFundamentals2/script.js
+++ jsFundamentals2/script.js
@@ -93,38 +93,208 @@
 
 //arrays
 
-const friends = ['Michael', 'Steven', 'Peter'];
+// const friends = ['Michael', 'Steven', 'Peter'];
 
-console.log(friends);
+// console.log(friends);
 
-const yearsY = new Array(1991, 1984, 2008, 2020);
-console.log(yearsY);
+// const yearsY = new Array(1991, 1984, 2008, 2020);
+// console.log(yearsY);
 
-console.log(friends[0]);
-console.log(friends.length);
-console.log(friends[friends.length - 1]);
+// console.log(friends[0]);
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
 
-friends[2] = 'Jay';
-console.log(friends);
+// friends[2] = 'Jay';
+// console.log(friends);
 
-const firstName = 'Jonas';
-const jonas = [firstName, 'Jonas', 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+// const firstName = 'Jonas';
+// const jonas = [firstName, 'Jonas', 'Schmedtmann', 2037 - 1991, 'teacher', friends];
 
-console.log(jonas);
-console.log(jonas[jonas.length - 1]);
+// console.log(jonas);
+// console.log(jonas[jonas.length - 1]);
 
-const calcAge = function (birthYear) {
-    return 2022 - birthYear;
+// const calcAge = function (birthYear) {
+//     return 2022 - birthYear;
+// }
+
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+
+// console.log(age1, age2, age3);
+
+// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+
+// console.log(ages);
+
+// const friends = ['Michael', 'Steven', 'Peter'];
+
+// //add elements to array
+// friends.push('Jay');
+
+// console.log(friends);
+
+// friends.unshift('John');
+
+// console.log(friends);
+
+// //remove elements to array
+
+// friends.pop();
+// const popped = friends.pop();
+// console.log(friends);
+
+// const unfriended = [popped];
+// console.log(`Unfriended: ${popped}`);
+
+// friends.shift();
+// console.log(friends);
+
+// console.log(friends.indexOf('Steven')); //returns the position of the element
+// console.log(friends.indexOf('Bob')); //if not in array will return -1
+
+// console.log(friends.includes('Steven')); //strict equality ===
+// console.log(friends.includes('Bob'));
+
+// friends.includes('Steven') ? console.log(`Friends`) : console.log(`Not friends`);
+
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+// 20%.
+// Your tasks:
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns
+// the corresponding tip, calculated based on the rules above (you can check out
+// the code from first tip calculator challenge if you need to). Use the function
+// type you like the most. Test the function using a bill value of 100
+// 2. And now let's use arrays! So create an array 'bills' containing the test data
+// below
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from
+// the function you created before
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+
+// Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can
+// actually be the returned value of a function! So you can just call a function as array
+// values (so don't store the tip values in separate variables first, but right in the new
+// array) �
+
+// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * .15 : bill * .20; //arrow function variable = parameter => condition ? true : false
+
+// const bill = [125, 555, 44];
+// const tips = [calcTip(bill[0]),calcTip(bill[1]), calcTip(bill[2])];
+// console.log(tips);
+
+// const total = [bill[0] + tips[0], bill[1] + tips[1], bill[2] + tips[2]];
+// console.log(total);
+
+//objects = format = variable = { properties: 'value', } Object Literal Syntax
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schmendtmann',
+//     age: 2037 - 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven']
+// };
+
+// console.log(jonas);
+// console.log(`${jonas.firstName} is ${jonas.age} years old ${jonas.job}.`);
+
+// const nameKey = 'Name';
+// console.log(`${jonas['first' + nameKey]} is ${jonas['age']} years old ${jonas['job']}.`);
+
+
+// let userChoice = prompt("What do you want to know about Jonas? Choose between firstName, lastName, age, job, friends");
+
+// console.log(`Here's the info you need: ${jonas[userChoice]}`);
+
+// jonas.location = 'Portugal';
+// jonas['twitter'] = '@jonasschmedtman';
+// console.log(jonas);
+
+// console.log(`${jonas.firstName} has ${jonas['friends'].length} friends, and his bestfriend is ${jonas.friends[0]}.`);
+
+//object methods
+
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schmendtmann',
+//     birthYear: 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven'],
+//     hasDriverLicense: true,
+//     //using function as key and value pair
+//     // calcAge: function(birthYear) {
+//     //     return 2022 - birthYear;
+//     // }
+//     // calcAge: function () {
+//     //     return 2037 - this.birthYear;
+//     // }
+//     //any function that is attached to an object is a METHOD
+//     calcAge: function (){
+//         this.age = 2037 - this.birthYear;
+//         return this.age;
+//     },
+
+//     getSummary: function(){
+//         return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this['hasDriverLicense'] ? 'a' : 'no'} drivers license.`
+//     }
+// };
+
+// console.log(jonas.calcAge());
+
+// console.log(jonas.age)
+//console.log(jonas['calcAge'](1993)); //using bracket notation
+
+// console.log(`${jonas.firstName} is a ${jonas.calcAge()}-year old ${jonas.job}, and he has ${jonas['hasDriverLicense'] ? 'a' : 'no'} drivers license.`)
+
+// console.log(jonas.getSummary());
+
+// Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+// implement the calculations! Remember: BMI = mass / height ** 2 = mass
+// / (height * height) (mass in kg and height in meter)
+// Your tasks:
+// 1. For each of them, create an object with properties for their full name, mass, and
+// height (Mark Miller and John Smith)
+// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+// method on both objects). Store the BMI value to a property, and also return it
+// from the method
+// 3. Log to the console who has the higher BMI, together with the full name and the
+// respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+// Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+// tall
+
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function (){
+        this.BMI = this.mass / (this.height ** 2);
+        return this.BMI
+    }
 }
 
-const years = [1990, 1967, 2002, 2010, 2018];
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function (){
+        this.BMI = this.mass / (this.height ** 2);
+        return this.BMI
+    }
+}
 
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age3 = calcAge(years[years.length - 1]);
+mark.calcBMI();
+john.calcBMI();
 
-console.log(age1, age2, age3);
-
-const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
-
-console.log(ages);
+// console.log(`${mark.BMI > john.BMI ? mark.fullName : john.fullName}'s BMI
+// (${mark.BMI > john.BMI ? mark.BMI : john.BMI}) is higher than
+// `)
+console.log(`-------------------------------`)
+console.log(`${mark.BMI > john.BMI
+            ? `${mark.fullName}'s BMI ${mark.BMI} is higher than ${john.fullName}'s BMI ${john.BMI}.`
+            : `${john.fullName}'s BMI ${john.BMI} is higher than ${mark.fullName}'s BMI ${mark.BMI}.`
+}
+`
+);
