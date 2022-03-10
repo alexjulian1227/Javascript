dd52808d305fc0bfbc3301df5209a362009f0ece
Alex Julian <alexjulian1227@gmail.com>
2022-03-10 11:43:26 +0800

    Progress data structures JS

--- dataStructure/script.js
+++ dataStructure/script.js
@@ -141,21 +141,117 @@ const game = {
 //map like objects use key and value
 //map keys can have any type unlike objects that keys can only be strings
 
-const rest = new Map();
-rest.set("name", "Classico Italiano");
-rest.set(1, "Firenze, Italy");
-rest.set(2, "Lisbon, Portugal");
-rest
-  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
-  .set("open", 11)
-  .set("close", 23)
-  .set(true, "We are open")
-  .set(false, "We are close");
+// const rest = new Map();
+// rest.set("name", "Classico Italiano");
+// rest.set(1, "Firenze, Italy");
+// rest.set(2, "Lisbon, Portugal");
+// rest
+//   .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+//   .set("open", 11)
+//   .set("close", 23)
+//   .set(true, "We are open")
+//   .set(false, "We are close");
 
-console.log(rest.get("name"));
-console.log(rest.get(true));
+// console.log(rest.get("name"));
+// console.log(rest.get(true));
 
-console.log(rest);
+// console.log(rest);
+
+// const time = 8;
+// console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+// console.log(rest.has("categories"));
+
+// rest.delete(2);
+// console.log(rest);
+// console.log(rest.size);
+
+// const arr = [1, 2];
+// rest.set(arr, "Test");
+// rest.set(document.querySelector("h1"), "Heading");
+// console.log(rest);
+
+// console.log(rest.get(arr));
+
+const question = new Map([
+  ["question", "What is the best programming language in the world?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "Javascript"],
+  ["correct", 3],
+  [true, "Correct!"],
+  [false, "Try again"],
+]);
+const gameEvents = new Map([
+  [17, "GOAL"],
+  [36, "Substitution"],
+  [47, "GOAL"],
+  [61, "Substitution"],
+  [64, "Yellow card"],
+  [69, "Red card"],
+  [70, "Substitution"],
+  [72, "Substitution"],
+  [76, "GOAL"],
+  [80, "GOAL"],
+  [92, "Yellow card"],
+]);
+//1
+// const [events] = new Set(gameEvents.entries());
+// console.log([...events]);
+// console.log(typeof events);
+const events = gameEvents.entries();
+console.log(events);
+
+for (const [key, value] of events) {
+  key <= 45
+    ? console.log(`[FIRST HALF] ${key}: ${value}`)
+    : console.log(`[SECOND HALF] ${key}: ${value}`);
+}
+//2
+// gameEvents.delete(64);
+// console.log(gameEvents);
+//3
+//"An event happened, on average, every 9 minutes"
+// const eventsInterval = [...gameEvents.keys()];
+
+// console.log(
+//   `An event happened, on average, every ${90 / eventsInterval.length} minutes`
+// );
+//4
+
+// Let's continue with our football betting app! This time, we have a map called
+// 'gameEvents' (see below) with a log of the events that happened during the
+// game. The values are the events themselves, and the keys are the minutes in which
+// each event happened (a football game has 90 minutes plus some extra time).
+// Your tasks:
+// 1. Create an array 'events' of the different game events that happened (no
+// duplicates)
+// 2. After the game has finished, is was found that the yellow card from minute 64
+// was unfair. So remove this event from the game events log.
+// 3. Compute and log the following string to the console: "An event happened, on
+// average, every 9 minutes" (keep in mind that a game has 90 minutes)
+// 4. Loop over 'gameEvents' and log each element to the console, marking
+// whether it's in the first half or second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17: ⚽ GOAL
+
+// console.log(question);
+
+// //convert obj to maps
+// const hoursMap = new Map(Object.entries(openingHours));
+// // console.log(hoursMap);
+// console.log(question.get("question"));
+// for (const [key, value] of question) {
+//   if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+// }
+
+// const answer = Number(prompt("Your answer : "));
+// console.log(question.get(question.get("correct") === answer));
+//convert map to array
+// console.log([...question]);
+// console.log(question.entries());
+// console.log(question.keys());
+// console.log(question.values());
+
 //set iterables with NO DUPLICATES
 //no order/index like arrays
 // const orderSet = new Set(["Pasta", "Pizza", "Pizza", "Risotto", "Pasta"]);
