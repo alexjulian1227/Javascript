6f6395b44e2229a2dd44c9b81c0ae62eb3f68280
Alex Julian <alexjulian1227@gmail.com>
2022-03-14 12:25:51 +0800

    Progress dates js

--- arrayBankistv2/script.js
+++ arrayBankistv2/script.js
@@ -81,19 +81,28 @@ const inputClosePin = document.querySelector(".form__input--pin");
 /////////////////////////////////////////////////
 // Functions
 
-const displayMovements = function (movements, sort = false) {
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = "";
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
+    const date = new Date(acc.movementsDates[i]);
 
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth()}`.padStart(2, 0);
+    const year = date.getFullYear();
+
+    const displayDate = `${day}/${month}/${year}`;
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
+      <div class="movements__date">${displayDate}</div>
         <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
@@ -142,7 +151,7 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
   // Display movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   // Display balance
   calcDisplayBalance(acc);
@@ -154,6 +163,20 @@ const updateUI = function (acc) {
 ///////////////////////////////////////
 // Event handlers
 let currentAccount;
+
+//ALWAYS LOGGED IN
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
+
+const now = new Date();
+const date = `${now.getDate()}`.padStart(2, 0);
+const month = `${now.getMonth()}`.padStart(2, 0);
+const year = now.getFullYear();
+const hour = `${now.getHours()}`.padStart(2, 0);
+const min = `${now.getMinutes()}`.padStart(2, 0);
+
+labelDate.textContent = `${date}/${month}/${year}, ${hour}:${min}`;
 
 btnLogin.addEventListener("click", function (e) {
   // Prevent form from submitting
@@ -197,6 +220,8 @@ btnTransfer.addEventListener("click", function (e) {
     // Doing the transfer
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
 
     // Update UI
     updateUI(currentAccount);
@@ -214,6 +239,7 @@ btnLoan.addEventListener("click", function (e) {
   ) {
     // Add movement
     currentAccount.movements.push(amount);
+    currentAccount.movementsDates.push(new Date().toISOString());
 
     // Update UI
     updateUI(currentAccount);
@@ -254,15 +280,48 @@ btnSort.addEventListener("click", function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+//Dates and Times
+//create a date 4ways
+// const now = new Date();
+// console.log(now);
+
+// console.log(new Date("Mar 14 2022 10:35:35"));
+// console.log(new Date("December 24, 2021"));
+// console.log(account1);
+// console.log(new Date(account1.movementsDates[0]));
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000)); //milliseconds to days
+
+//working with dates
+// const future = new Date(2037, 10, 19, 15, 23, 5);
+// console.log(future.getFullYear());
+// console.log(future.getMonth());
+// console.log(future.getDate());
+// console.log(future.getDay());
+// console.log(future.getHours());
+// console.log(future.getMinutes());
+// console.log(future.getSeconds());
+// console.log(future.toISOString());
+// console.log(future.toDateString());
+// console.log(future.getTime());
+// console.log(new Date(2142228185000)); //timestamp
+// console.log(Date.now()); //get the timestamp right now
+
+// future.setFullYear(2040); //modify the date and will auto calculate the rest
+// console.log(future);
 //remainder
-console.log(5 % 2);
-console.log(8 % 3);
+//BIG INT
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(BigInt(4938490234));
+// console.log(234234234234234234n);
+// console.log(5 % 2);
+// console.log(8 % 3);
 
-console.log(6 % 2);
-console.log(7 % 2);
+// console.log(6 % 2);
+// console.log(7 % 2);
 
-const isEven = (num) => num % 2 === 0; //checks if num is even
-console.log(isEven(7));
+// const isEven = (num) => num % 2 === 0; //checks if num is even
+// console.log(isEven(7));
 
 // labelBalance.addEventListener("click", function () {
 //   [...document.querySelectorAll(".movements__row")].forEach(function (row, i) {
