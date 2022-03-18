710b43096c29b138780812befc329b3700fd8d17
Alex Julian <alexjulian1227@gmail.com>
2022-03-18 13:44:04 +0800

    Progress advance DOM

--- advanceDOM/script.js
+++ advanceDOM/script.js
@@ -62,6 +62,27 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
   }
 });
 
+//TABBED COMPONENT
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  console.log(clicked);
+
+  if (!clicked) return; //guard clause
+  //active tab
+  tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+  clicked.classList.add("operations__tab--active");
+  //active content
+  tabsContent.forEach((tab) =>
+    tab.classList.remove("operations__content--active")
+  );
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
 //TYPE OF EVENTS AND EVENT HANDLERS
 // const h1 = document.querySelector("h1");
 
