4b3b33069537c5ff69efc86477db2baccf5f1f9c
Alex Julian <alexjulian1227@gmail.com>
2022-03-19 07:01:27 +0800

    Progress oop js

--- oop/script.js
+++ oop/script.js
@@ -1,1 +1,34 @@
-'use strict';
+"use strict";
+
+//constructors
+const Person = function (firstName, birthYear) {
+  //instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const jonas = new Person("Jonas", 1991);
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 2010);
+console.log(jonas);
+console.log(matilda);
+console.log(jack);
+
+//new {} obj is created
+//the func is called , this = {}
+//{} linked to prototype
+//automatically returned to the constructor {}
+
+//prototype
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(Person));
