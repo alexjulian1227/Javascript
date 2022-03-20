0cbc537cc517175cc6a48d06ade3f336d9207518
Alex Julian <alexjulian1227@gmail.com>
2022-03-20 18:42:47 +0800

    Progress mapty app

--- mapty/script.js
+++ mapty/script.js
@@ -1,12 +1,42 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    //https://www.google.com/maps/@15.7607,119.916227
+    const a = 15.756101302653137;
+    const b = 119.91078794002534;
+    const coords = [a, b];
+    const map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    map.on("click", function (e) {
+      const { lat, lng } = e.latlng;
+      console.log(lat, lng);
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup("Your clicked here")
+        .openPopup();
+    });
+  },
+  function () {
+    alert(`Could not get your current position!`);
+  }
+);
