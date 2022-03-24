a12eda86a32cf98500eaab7e6bc2e971fc97616a
Alex Julian <alexjulian1227@gmail.com>
2022-03-25 06:11:53 +0800

    Finished promise js need more examples

--- ajax/script.js
+++ ajax/script.js
@@ -17,17 +17,222 @@ const createImage = function (imgPath) {
     });
   });
 };
-createImage("img/img-1.jpg").then((img) => {
-  console.log(`Image 1 loaded.`);
-});
+
+let currentImg;
+
+// const imgArr = ["img/img-1.jpg", "img/img-2.jpg", "img/img-3.jpg"];
+
+const loadNPause = async function () {
+  try {
+    let img = await createImage("img/img-1.jpg");
+    await wait(2);
+
+    img.style.display = "none";
+    img = await createImage("img/img-2.jpg");
+
+    await wait(2);
+    img.style.display = "none";
+  } catch (err) {
+    console.log(err);
+  }
+};
+// loadNPause("img/img-1.jpg");
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async (img) => await createImage(img));
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+    imgsEl.forEach((img) => img.classList.add("parallel"));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+loadAll(["img/img-1.jpg", "img/img-2.jpg", "img/img-3.jpg"]);
+// createImage("img/img-1.jpg")
+//   .then((img) => {
+//     console.log(`Image 1 loaded.`);
+//     currentImg = img;
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = "none";
+//     return createImage("img/img-2.jpg");
+//   })
+//   .then((img) => {
+//     currentImg = img;
+//     console.log(`Image 2 loaded.`);
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImage.style.display = "none";
+//   })
+//   .catch((err) => console.error(err));
 
 const wait = function (seconds) {
   return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 };
 
-wait(2).then(() => {
-  imgContainer.style.display = "none";
-});
+//other promise combinator note than if one of the promise fails others will shortcircuit
+//promise.race needs an array of promise and immediately return a promise once one of the promise value is available and doesnt matter if it's resolve or rejected
+
+// const getJSON = function (url, errorMsg = "Something went wrong") {
+//   return fetch(url).then((response) => {
+//     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+//     return response.json();
+//   });
+// };
+
+// (async function () {
+//   const response = await Promise.race([
+//     getJSON(`https://restcountries.com/v2/name/italy`),
+//     getJSON(`https://restcountries.com/v2/name/egypt`),
+//     getJSON(`https://restcountries.com/v2/name/mexico`),
+//   ]);
+//   console.log(response[0]);
+// })();
+
+// const timeout = function (sec) {
+//   //if the user has slow connection
+//   return new Promise(function (_, reject) {
+//     setTimeout(function () {
+//       reject(new Error(`request took too long`));
+//     }, sec * 1000);
+//   });
+// };
+// Promise.race([
+//   getJSON(`https://restcountries.com/v2/name/philippines`),
+//   timeout(0.1),
+// ])
+//   .then((response) => console.log(response[0]))
+//   .catch((err) => console.log(err));
+
+//Promise.allSettle does not short circuit if one promise fails and returns all promise
+// Promise.allSettled([
+//   Promise.resolve("Success"),
+//   Promise.reject("Fails"),
+//   Promise.resolve("AnotherSuccess"),
+//   Promise.resolve("ERROR"),
+// ]).then((response) => console.log(response));
+
+//promise.any[ES2021] will return the first fullfilled promise and rejected promise are ignored//  ALWAYS RETURN FULFILLED PROMISE
+// Promise.any([
+//   Promise.resolve("Success"),
+//   Promise.reject("Fails"),
+//   Promise.resolve("AnotherSuccess"),
+//   Promise.resolve("ERROR"),
+// ]).then((response) => console.log(response));
+
+//Promise.all
+// const get3Countries = async function (c1, c2, c3) {
+//   try {
+//     //this await will run in the background 3 different times
+//     // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+//     // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+//     // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+
+//     //this should be it running in parallel
+//     const data = await Promise.all([
+//       getJSON(`https://restcountries.com/v2/name/${c1}`),
+//       getJSON(`https://restcountries.com/v2/name/${c2}`),
+//       getJSON(`https://restcountries.com/v2/name/${c3}`),
+//     ]);
+//     console.log(data.map((d) => d[0].capital));
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// get3Countries("portugal", "canada", "philippines");
+// const getPosition = function () {
+//   return new Promise(function (resolve, reject) {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
+// // getPosition()
+// //   .then((pos) => console.log(pos))
+
+// const startApp = function () {
+//   navigator.geolocation.getCurrentPosition(function (pos) {
+//     const { latitude: lat, longitude: long } = pos.coords;
+//     getCountry(lat, long);
+//   });
+// };
+// const getCountry = async function (lat, long) {
+//   const res = await fetch(`https://geocode.xyz/${lat},${long}?geoit=json`);
+//   if (!res.ok) throw new Error("Problem getting the country using lat long");
+//   const data = await res.json();
+
+//   whereAmI(data.country);
+// };
+
+// const whereAmI = async function (country) {
+//   try {
+//     const res = await fetch(`https://restcountries.com/v2/name/${country}`);
+//     const data = await res.json();
+
+//     renderGetCountry(data[0]);
+//     console.log(`Finished getting the data needed`);
+//   } catch (err) {
+//     console.error(`Something went wrong: ${err}`);
+//   }
+// };
+// startApp();
+// console.log(`test`);
+
+// (async function(){
+// try{
+//     const city = await whereAmI()
+//     console.log(`2: ${city}`)
+
+// } catch{
+//     console.error(`2: ${err.message}`)
+// }
+//     console.log(`3. Finished getting location`))()
+// const imgContainer = document.querySelector(".images");
+
+// const createImage = function (imgPath) {
+//   return new Promise(function (resolve, reject) {
+//     const img = document.createElement("img");
+//     img.src = imgPath;
+
+//     img.addEventListener("load", function () {
+//       imgContainer.append(img);
+//       resolve(img);
+//     });
+
+//     img.addEventListener("error", function () {
+//       reject(new Error("Image not found."));
+//     });
+//   });
+// };
+
+// let currentImg;
+
+// createImage("img/img-1.jpg")
+//   .then((img) => {
+//     console.log(`Image 1 loaded.`);
+//     currentImg = img;
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = "none";
+//     return createImage("img/img-2.jpg");
+//   })
+//   .then((img) => {
+//     currentImg = img;
+//     console.log(`Image 2 loaded.`);
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImage.style.display = "none";
+//   })
+//   .catch((err) => console.error(err));
+
+// const wait = function (seconds) {
+//   return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+// };
+
 // const lotteryPromise = new Promise(function (resolve, reject) {
 //   console.log(`Lottery draw is now ongoing....`);
 
