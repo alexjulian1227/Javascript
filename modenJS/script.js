7edc39012bf9b78dbb6d6ec29c06d615861de6e7
Alex Julian <alexjulian1227@gmail.com>
2022-03-26 01:45:26 +0800

    Progress in modern modules js

--- modenJS/script.js
+++ modenJS/script.js
@@ -1,1 +1,24 @@
-import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+// import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+import cloneDeep from "lodash-es";
+
+const state = {
+  cart: [
+    { product: "bread", quantity: 5 },
+    { product: "pizza", quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+
+console.log(stateClone);
+
+console.log(stateDeepClone);
+
+if (module.hot) {
+  //updating inside the modules will the web on its current status or keep whats in doing unlike logging in to the page everytime
+  module.hot.accept();
+}
